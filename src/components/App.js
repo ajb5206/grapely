2da28fb49d2815bd91428f82a7e6a3d524372d1a
Alex Bertotto <ajb5206@gmail.com>
2022-03-06 17:45:37 -0800

    properly set up app.css page

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 import Header from './Header';
-import '../App.css';
+import '.././App.css';
 import React from 'react';
+import SearchBar from './SearchBar';
 
 function App() {
   return (
     <React.Fragment>
+      <div className="App">
       <Header />
-
+      <SearchBar />
+      </div>
     </React.Fragment> 
   );
 }
