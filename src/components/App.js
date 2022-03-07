61f6531b7494173a0e2347d907d51d71299a921c
Alex Bertotto <ajb5206@gmail.com>
2022-03-06 16:11:32 -0800

    add components folder and basic header component

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import Header from './Header';
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header />
+      
+    </React.Fragment> 
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header(){
+  return (
+		<React.Fragment>
+    <h1>Grapely</h1>
+		<h4>Grape knowledge for your wine journey!</h4>
+		</React.Fragment>
+  );
+}
+
+export default Header;
