5bebebe818c60e869df261e677ac862714f8c513
Alex Bertotto <ajb5206@gmail.com>
2022-03-07 20:29:38 -0800

    added useState so that the search bar will filter through the data.json and return proper results

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,20 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SearchBar.css";
 
 
 function SearchBar({placeholder, data}){
+
+	const [filteredData, setFilteredData] = useState([]);
+
+
+	const handleFilter = (event) => {
+		const searchWord = event.target.value
+		const newFilter = data.filter((value) => {
+			return value.grapeName.toLowerCase().includes(searchWord.toLowerCase());
+		});
+		setFilteredData(newFilter);  
+	};
+
   return (
-		<React.Fragment className="search">
+		<div className="search">
 			<div className="searchInputs">
-				<input type="text" placeholder={placeholder}/>
+				<input type="text" placeholder={placeholder} 
+					onChange={handleFilter}/>
 				</div>
+			{filteredData.length !== 0 && (
 			<div className="dataResult">
-				{data.map((value, key) => {
+				{filteredData.map((value, key) => {
 					return <div className="searchResults"> {value.grapeName}</div> // maybe customize tag or use a tag for clickable? <a grapeName="dataItem" href={value.COMPONENT?} target="_blank">
 				})}
 			</div>
-    
-		</React.Fragment>
+			)}
+		</div>
   );
 }
 
