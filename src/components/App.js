7322bf50b1621d72c36a1cc8b80b489d5ad933f8
Alex Bertotto <ajb5206@gmail.com>
2022-03-07 19:28:44 -0800

    set up basic data in data.json and link said data to searchbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,15 @@
 import Header from './Header';
-import '.././App.css';
+import '../App.css';
 import React from 'react';
 import SearchBar from './SearchBar';
+import GrapeData from "../Data.json"
 
 function App() {
   return (
     <React.Fragment>
       <div className="App">
       <Header />
-      <SearchBar />
+      <SearchBar placeholder="Enter a grape..." data={GrapeData} />
       </div>
     </React.Fragment> 
   );
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,16 @@
 import React from "react";
 
-function SearchBar(){
+function SearchBar({placeholder, data}){
   return (
-		<React.Fragment>
+		<React.Fragment className="search">
+			<div className="searchInputs">
+				<input type="text" placeholder={placeholder}/>
+				</div>
+			<div className="dataResult">
+				{data.map((value, key) => {
+					return <div> {value.grapeName}</div> // maybe customize tag or use a tag for clickable?
+				})}
+			</div>
     
 		</React.Fragment>
   );
