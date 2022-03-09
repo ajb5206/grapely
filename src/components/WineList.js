c88f1a65f0aefca36bf307dc5638a2359fef2773
Alex Bertotto <ajb5206@gmail.com>
2022-03-09 12:54:28 -0800

    update winelist to contain basic information

--- src/components/WineList.js
+++ src/components/WineList.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import Wine from './Wine';
 
-function WineList() {
+function WineList(props) {
+
+
 	return (
 		<div>
+			<hr />
+			{props.wineList.map((wine) =>
+			<Wine 
+				whenWineClicked = {props.onWineSelection}
+				name={wine.name}
+				region={wine.region}
+				varietals={wine.varietals}
+				notes={wine.notes} 
+				id={wine.id} />
 
+				)}
 		</div>
-	)
+	);
 }
 
 export default WineList;
