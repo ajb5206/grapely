d88d334b87900e5f8eccc5129e42f7b189652d31
Alex Bertotto <ajb5206@gmail.com>
2022-03-10 13:04:45 -0800

    add proptypes and update props

--- src/components/WineDetail.js
+++ src/components/WineDetail.js
@@ -1,8 +1,9 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 
 function WineDetail(props){
-  const { wine } = props;
+  const { wine, onClickingDelete } = props;
 
   return (
     <React.Fragment>
@@ -11,9 +12,17 @@ function WineDetail(props){
       <p>{wine.region}</p>
 			<p>{wine.varietals}</p>
 			<p>{wine.notes}</p>
+			<button onClick= {props.onClickingEdit }>Update Wine</button>
+			<button onClick={() => onClickingDelete(wine.id) }>Close Wine</button>
       <hr/>
     </React.Fragment>
   );
 }
+
+WineDetail.propTypes = {
+	wine: PropTypes.object,
+	onClickingDelete: PropTypes.func,
+	onClickingEdit: PropTypes.func
+};
 
 export default WineDetail;
