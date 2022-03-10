6a2aab182e1e848ecef399c2405c0a171380a627
Alex Bertotto <ajb5206@gmail.com>
2022-03-10 12:43:20 -0800

    add index.js for actions

---
+++ src/actions/index.js
@@ -0,0 +1,22 @@
+import * as c from './ActionTypes';
+
+export const deleteWine = id => ({
+	type: c.DELETE_WINE,
+	id
+});
+
+export const toggleForm = () => ({
+	type: c.TOGGLE_FORM
+});
+
+export const addWine = (wine) => {
+	const { name, region, varietals, notes, id } = wine;
+	return{
+		type: c.ADD_WINE,
+		name: name,
+		region: region,
+		varietals: varietals,
+		notes: notes,
+		id: id,
+	};
+};
