991446ecf25ce822518af1d44f6cf305536987c6
Alex Bertotto <ajb5206@gmail.com>
2022-03-10 12:39:33 -0800

    add firestore store to newwineform

--- src/components/NewWineForm.js
+++ src/components/NewWineForm.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import ReusableForm from './ReusableForm';
+import { useFirestore } from 'react-redux-firebase';
 
 function NewWineForm(props){
 
+	const firestore = useFirestore();
+
+	function addWineToFirestore(event) {
+		event.preventDefault();
+		props.onNewWineCreation();
+
+		return firestore.collection('wine').add(
+      {
+        name: event.target.name.value,
+        region: event.target.region.value,
+				varietals: event.target.varietals.value, 
+        notes: event.target.notes.value,
+        timeOpen: firestore.FieldValue.serverTimestamp()
+      }
+    );
+	}
 
 	return (
 		<React.Fragment>
 			<ReusableForm	
-				formSubmissionHandler={addWine_______}
+				formSubmissionHandler={addWineToFirestore}
 				buttonText="Submit!" />
 		</React.Fragment>
 	);
--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import ReactDom from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { ReactReduxFirebaseProvider } from 'react-redux-firebase';
+import { createFirestoreInstance } from 'redux-firestore';
+import firebase from "./firebase";
+
+const store = createStore(rootReducer);
+
+const rrfProps = {
+  firebase,
+  config: {
+    userProfile: "users"
+  },
+  dispatch: store.dispatch,
+  createFirestoreInstance
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <ReactReduxFirebaseProvider {...rrfProps}>
+      <App />
+    </ReactReduxFirebaseProvider>
+  </Provider>,
   document.getElementById('root')
 );
 
