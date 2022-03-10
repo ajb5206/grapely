b3b5590d1fcba6d2f99bfaa54438424ff1cbd771
Alex Bertotto <ajb5206@gmail.com>
2022-03-10 13:09:26 -0800

    update with firestoreconnect and isloaded and proptypes

--- src/components/WineList.js
+++ src/components/WineList.js
@@ -1,24 +1,45 @@
 import React from 'react'
 import Wine from './Wine';
+import PropTypes from "prop-types";
+import { useSelector } from 'react-redux';
+import { useFirestoreConnect, isLoaded, isEmpty } from 'react-redux-firebase';
 
 function WineList(props) {
 
+		useFirestoreConnect([
+			{ collection: 'wines' }
+		]);
+
+		const wines = useSelector(state => state.firestore.ordered.wines);
+
+		if (isLoaded(wines)) {
+			return (
+				<React.Fragment>
+					<hr/>
+					{wines.map((wine) => {
+						return <Wine 
+						whenWineClicked = { props.onWineSelection }	
+						name={wine.name}
+						region={wine.region}
+						varietals={wine.varietals}
+						notes={wine.notes}
+						id={wine.id}
+						key={wine.id}/>
+					})}
+				</React.Fragment>
+			);
+		} else {
 
 	return (
-		<div>
-			<hr />
-			{props.wineList.map((wine) =>
-			<Wine 
-				whenWineClicked = {props.onWineSelection}
-				name={wine.name}
-				region={wine.region}
-				varietals={wine.varietals}
-				notes={wine.notes} 
-				id={wine.id} />
-
-				)}
-		</div>
-	);
+		<React.Fragment>
+			<h3>Loading ....</h3>
+		</React.Fragment>
+		);
+	}
 }
+
+WineList.propTypes = {
+	onTicketSelection: PropTypes.func
+};
 
 export default WineList;
