94c6c68cd865cd79140301d916622b14967e1741
Alex Bertotto <ajb5206@gmail.com>
2022-03-10 12:52:52 -0800

    update methods

--- src/components/WineControl.js
+++ src/components/WineControl.js
@@ -19,10 +19,53 @@ class WineControl extends React.Component {
 		};
 	}
 
-	// handleClick = () => {
-	// 	if (this.state.selectedWine)
-	// }
+	handleClick = () => {
+		if (this.state.selectedWine != null) {
+			this.setState({
+				selectedWine: null,
+				editing: false
+			});
+		} else {
+			const { dispatch } = this.props;
+			const action = a.toggleForm();
+		dispatch(action);
+		}
+	}
 
+	handleAddingNewWineToList = () => {
+		const { dispatch } = this.props;
+		const action = a.toggleForm();
+		dispatch(action);
+	}
+
+	handleChangingSelectedWine = (id) => {
+		this.props.firestore.get({collection: 'wines', doc: id}).then((wine) => {
+			const firestoreWine = {
+				name: wine.get("name"),
+				region: wine.get("region"),
+				varietals: wine.get("varietals"),
+				notes: wine.get("notes"),
+				id: wine.id
+			}
+			this.setState({selectedWine: firestoreWine });
+		});
+	}
+
+	handleDeletingWine = (id) => {
+		this.props.firestore.delete({collection: 'wines', doc: id});
+		this.setState({selectedWine: null})
+	}
+
+	handleEditClick = () => {
+		this.setState({editing: true});
+	}
+
+	handleEditingWineInList = () => {
+		this.setState({
+			editing: false,
+			selectedWine: null
+		});
+	}
 
 	render(){
 		let currentlyVisibleState = null;
@@ -58,4 +101,16 @@ class WineControl extends React.Component {
 	}
 }
 
-export default WineControl;
+WineControl.propTypes = {
+	formVisibleOnPage: PropTypes.bool
+};
+
+const mapStateToProps = state => {
+	return {
+		formVisibleOnPage: state.formVisibleOnPage
+	}
+}
+
+WineControl = connect(mapStateToProps)(WineControl);
+
+export default withFirestore(WineControl);
