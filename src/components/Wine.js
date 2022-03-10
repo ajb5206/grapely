7c58e7a9eaa61d6dc72445eb1c4710ddd51e815e
Alex Bertotto <ajb5206@gmail.com>
2022-03-10 12:27:55 -0800

    add firebase, firestore, react-redux-firebase, and redux-firestore

--- src/components/Wine.js
+++ src/components/Wine.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 
 function Wine(props) {
 	return (
 		<div>
-			<div onCLick = {() => props.whenWineClicked(props.id)}>
+			<div onClick = {() => props.whenWineClicked(props.id)}>
 				<h3>{props.name}</h3>	
 				<p>{props.region}</p>
 				<p>{props.varietals}</p>
@@ -13,6 +14,12 @@ function Wine(props) {
 			</div>
 		</div>
 	);
+}
+
+Wine.propTypes = {
+	name: PropTypes.string,
+	region: PropTypes.string,
+	varietals: PropTypes.string
 }
 
 export default Wine;
