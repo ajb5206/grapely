18633597fb67cb5c6b400aa6ab896a44b19f82b1
Alex Bertotto <ajb5206@gmail.com>
2022-03-12 13:22:38 -0800

    change useHistory to useNavigate

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -12,7 +12,11 @@ export function AuthProvider({ children }) {
 	const [loading, setLoading] = useState(true)
 
 	function signup(email, password) {
-		auth.createUserWithEmailAndPassword(email, password)
+		return auth.createUserWithEmailAndPassword(email, password)
+	}
+
+	function login(email, password) {
+		return auth.signInWithEmailAndPassword(email, password)
 	}
 
 	useEffect(() => {
@@ -25,6 +29,7 @@ export function AuthProvider({ children }) {
 	
 	const value = {
 		currentUser,
+		login,
 		signup
 	}
 	return (
