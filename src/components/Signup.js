545e1a59443dc6326b257e914b10e6cab651980c
Alex Bertotto <ajb5206@gmail.com>
2022-03-12 12:18:36 -0800

    set up loading error capabilities and add bootstrap Alert

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from 'react';
-import { Form, Button, Card } from 'react-bootstrap';
+import { Form, Button, Card, Alert } from 'react-bootstrap';
 import { useAuth } from '../contexts/AuthContext'
 
 export default function Signup() {
@@ -33,7 +33,8 @@ export default function Signup() {
 			<Card>
 				<Card.Body>
 					<h2 className="text-center mb-4">Sign Up</h2>
-					<Form>
+					{error && <Alert variant="danger">{error}</Alert>}
+					<Form onSubmit={handleSubmit}>
 						<Form.Group id="email">
 							<Form.Label>Email</Form.Label>
 							<Form.Control type="email" ref={emailRef} required />
@@ -46,7 +47,7 @@ export default function Signup() {
 							<Form.Label>Password Confirmation</Form.Label>
 							<Form.Control type="p" ref={passwordConfirmRef} required />
 						</Form.Group>
-						<Button className="w-100" type="submit">
+						<Button disabled={loading} className="w-100" type="submit">
 							Sign Up
 							</Button>
 					</Form>
