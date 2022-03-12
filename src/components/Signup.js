fd13ac0663e5f32c95d671bb211d8dac622f697b
Alex Bertotto <ajb5206@gmail.com>
2022-03-12 12:16:02 -0800

    add async function to handle submit with error and loading handled by useState

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -7,17 +7,26 @@ export default function Signup() {
 	const passwordRef = useRef();
 	const passwordConfirmRef = useRef();
 	const { signup } = useAuth();
-	const [error, setError] = useState('')
+	const [error, setError] = useState('');
+	const [loading, setLoading] = useState(false);
 
-	function handleSubmit(event) {
+	async function handleSubmit(event) {
 		event.preventDefault()
 
 		if(passwordRef.current.value !== passwordConfirmRef.current.value) {
 			return setError('Passwords do not match')
 		}
 
-		signup(emailRef.current.value, passwordRef.current.value)
-	} 
+		try {
+			setError('')
+			setLoading(true)
+		await signup(emailRef.current.value, passwordRef.current.value)
+	} catch {
+		setError('Failed to create an account')
+	}
+
+	setLoading(false)
+	}
 
 	return (
 		<div>
