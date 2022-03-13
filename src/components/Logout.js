085d337514e73a3a8187afbfe6ea7b53986f9e7f
Alex Bertotto <ajb5206@gmail.com>
2022-03-13 13:05:56 -0700

    add code to async func in hopes of making it work correctly, failed

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,28 +1,41 @@
 import React, { useState } from 'react';
 import { Button, Alert } from 'react-bootstrap';
 import { useAuth } from '../contexts/AuthContext';
-import { useHistory } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 function Logout() {
 	const [error, setError] = useState('');
 	const { logout } = useAuth();
-	const history = useHistory;
+	const navigate = useNavigate;
+	const [loading, setLoading] = useState(false);
 
-	async function handleLogout() {
-		setError('')
+	async function handleLogout(event) {
+		event.preventDefault()
+		
 
 		try {
+			setError('')
+			setLoading(true)
 			await logout()
-			history.push("/login")
+			navigate("/")
 		} catch {
 			setError("Failed to log out")
 		}
+	setLoading(false)
 	}
 
 	return (
 		<div>
-			{error && <Alert variant="danger">{error}</Alert>}
-			<Button variant="link" onClick={handleLogout}></Button>
+			<div>
+				{error && <Alert variant="danger">{error}</Alert>}
+				{/* <Button variant="link" onClick={handleLogout}></Button> */}
+				{/* <button onClick={handleLogout}></button> */}
+			</div>
+			<div className="w-100 text-center mt-2">
+        <Button  disabled={loading} variant="link" onClick={handleLogout}>
+          Log Out
+        </Button>
+      </div>
 		</div>
 	)
 }
