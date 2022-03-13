b7bfbbff1519440ec771ff31394a07939359e4d0
Alex Bertotto <ajb5206@gmail.com>
2022-03-13 09:16:05 -0700

    add bootstrap stylings

--- src/components/Grape.js
+++ src/components/Grape.js
@@ -1,7 +1,32 @@
-import React from 'react'
+import React from 'react';
+import { Card, Button } from 'react-bootstrap';
 
-export default function Grape() {
+export default function Grape(props) {
 	return (
-		<div>grape</div>
+		<div>
+			<Card>
+			<Card.Img src="{props.img}"></Card.Img>
+				<Card.Title>
+					{props.name}
+				</Card.Title>
+				<Card.Body>
+					{props.region}
+					{props.varietals}
+					{props.tastingNotes}
+				</Card.Body>
+			</Card>
+			<Card>
+				<Card.Img src="{props.img}"></Card.Img>
+				<Card.Title>
+					{props.name}
+				</Card.Title>
+				<Card.Subtitle>Suggested for you</Card.Subtitle>
+				<Card.Body>
+					{props.region}
+					{props.varietals}
+					{props.tastingNotes}
+				</Card.Body>
+			</Card>
+		</div>
 	)
 }
