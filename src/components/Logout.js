6c693fbe2fbc16649ebc1feff222b0ee27b3882a
Alex Bertotto <ajb5206@gmail.com>
2022-03-13 12:20:11 -0700

    start adding logout functionality with hooks

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from 'react-bootstrap';
+import { useAuth } from '../contexts/AuthContext';
+import { useHistory } from 'react-router-dom';
 
 function Logout() {
-	
+	const [error, setError] = useState('')
+	const { currentUser, logout } = useAuth();
+
+	async function handleLogout() {
+		setError('')
+
+		try {
+			await logout()
+		}
+	}
+
 	return (
 		<div>
 			<Button variant="link" onClick={handleLogout}></Button>
