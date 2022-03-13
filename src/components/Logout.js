6b021c862e52d4bc23d92f0772d57726401d89b6
Alex Bertotto <ajb5206@gmail.com>
2022-03-13 12:21:34 -0700

    add try catch block for error handling of logout

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -4,14 +4,18 @@ import { useAuth } from '../contexts/AuthContext';
 import { useHistory } from 'react-router-dom';
 
 function Logout() {
-	const [error, setError] = useState('')
+	const [error, setError] = useState('');
 	const { currentUser, logout } = useAuth();
+	const history = useHistory;
 
 	async function handleLogout() {
 		setError('')
 
 		try {
 			await logout()
+			history.push("/login")
+		} catch {
+			setError("Failed to log out")
 		}
 	}
 
